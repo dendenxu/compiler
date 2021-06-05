b253b1755eb92faa207c26aea57d7a73e30efd90
Asudy Wang <18906052155@189.cn>
2021-06-05 21:48:24 +0800

    test: strange pointer  **

--- samples/wjz.c
+++ samples/wjz.c
@@ -1,7 +1,19 @@
-int main(int argc, char **argv) {
-    int a = 1, b = 3, sum;
-    // sum = 0;
-    sum = a + b;
+#include <stdio.h>
 
-    return sum;
+int main() {
+    // int a = 1, b = 3, sum;
+    int a[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+    int ** aptr = (int**)a;
+    // sum = 0;
+    // sum = a + b;
+    printf("%d\n", a[0][0]);
+    printf("%d\n", a[1][1]);
+    printf("%d\n", *aptr);    // *(aptr + 1)
+    printf("%p, %p\n", &a[0], aptr + 1);
+
+    return 0;
 }
