4dd8a0497a92661fa81a7bf621e711dd954b965e
Asudy Wang <18906052155@189.cn>
2021-06-05 23:38:03 +0800

    test(parser): add parser test file

---
+++ samples/ParserTest.c
@@ -0,0 +1,28 @@
+// Multi-line Comment
+/**
+ * @file ParserTest.c
+ * This file gives test cases to all language elements
+ * (statements, expressions, etc.) in our parser. Each
+ * test case is marked by a one-line comment.
+ */
+
+
+// One-line Comment
+// Program
+
+
+// Global Declarations
+int a = 0;
+float f = 0.03;
+double pi2 = pi * 2;
+// declaration list with initialization
+char a, b, c = '1', d, e;
+char * array;
+char thisisaarry[19][20][21] = thisisanotherarray;
+// function 
+
+
+// Function 1
+int main(int argc, char *argv[]) {
+    
+}
